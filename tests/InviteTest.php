ab2737b024d040058cf1f55546f0810aceb5e591
Студент 9 Кабинет 409 <409-9@coopteh.local>
2024-10-26 16:00:56 +0700

    1134

--- tests/InviteTest.php
+++ tests/InviteTest.php
@@ -8,7 +8,7 @@ class InviteTest extends TestCase {
     public function test_router() {
         $router = new Router();
         $html = $router->route( "http://localhost/" );
-        $pos= mb_strpos($html, "Приглашаем в наше онлайн-кафе1");
+        $pos= mb_strpos($html, "Приглашаем в наше онлайн-кафе");
         $this->assertNotEquals(false, $pos);
     }
 }
